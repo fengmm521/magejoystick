3a1f6cc393de038d88da130318c37c0bfc813371
woodcol <fengmm521@gmail.com>
2020-08-01 12:43:16 +0800

    Update Gamepad_yaogan_rightMouse.ino

--- arduino/Gamepad_yaogan_rightMouse/Gamepad_yaogan_rightMouse.ino
+++ arduino/Gamepad_yaogan_rightMouse/Gamepad_yaogan_rightMouse.ino
@@ -20,7 +20,7 @@
  *       A0                                                       /
  *                    5(D2)   6(D4)   7(D6)   8(D8)              /    C1(D10) 
  *   A3       A1                                                / 
- *                    1(D3)   2(D5)   3(D7)   4(D9)            /    RESET
+ *                    1(D3)   2(D5)   3(D7)   4(D9)            /    c2(D16)
  *       A2                                                   /
  *                                                           /
  */
@@ -28,11 +28,11 @@
 const int btn1 = 3;
 const int btn2 = 5;
 const int btn3 = 7;
-const int btn4 = 9;
+const int MouseRight = 9;
 const int btn5 = 2;
 const int btn6 = 4;
 const int btn7 = 6;
-const int btn8 = 8;
+const int MouseLeft = 8;
 const int btnS1 = 14;
 const int btnS2 = 15;
 
@@ -46,15 +46,18 @@ const int bLeft = A3;
 
 uint32_t lastState = 0;
 
+bool isMouseRightUse = false;
+bool isMouseLeftUse = false;
+
 void setup() {
   pinMode(btn1, INPUT_PULLUP);
   pinMode(btn2, INPUT_PULLUP);
   pinMode(btn3, INPUT_PULLUP);
-  pinMode(btn4, INPUT_PULLUP);
+  pinMode(MouseRight, INPUT_PULLUP);
   pinMode(btn5, INPUT_PULLUP);
   pinMode(btn6, INPUT_PULLUP);
   pinMode(btn7, INPUT_PULLUP);
-  pinMode(btn8, INPUT_PULLUP);
+  pinMode(MouseLeft, INPUT_PULLUP);
   pinMode(btnS1, INPUT_PULLUP);
   pinMode(btnS2, INPUT_PULLUP);
   pinMode(btnC1, INPUT_PULLUP);
@@ -64,6 +67,8 @@ void setup() {
   pinMode(bRight, INPUT_PULLUP);
   pinMode(bDown, INPUT_PULLUP);
   pinMode(bLeft, INPUT_PULLUP);
+
+  
 
   // Sends a clean report to the host. This is important on any Arduino type.
   Gamepad.begin();
@@ -93,12 +98,14 @@ void loop() {
     Gamepad.release(3);
     nowState &= ~((uint32_t)1 << 3);
   }
-  if (!digitalRead(btn4)) {
-    Gamepad.press(4);
-    nowState |= (uint32_t)1 << 4; 
+  if (!digitalRead(MouseRight)) {
+//    Gamepad.press(4);
+//    nowState |= (uint32_t)1 << 4; 
+    isMouseRightUse = true;
   }else{
-    Gamepad.release(4);
-    nowState &= ~((uint32_t)1 << 4);
+//    Gamepad.release(4);
+//    nowState &= ~((uint32_t)1 << 4);
+    isMouseRightUse = false;
   }
   if (!digitalRead(btn5)) {
     Gamepad.press(5);
@@ -121,12 +128,14 @@ void loop() {
     Gamepad.release(7);
     nowState &= ~((uint32_t)1 << 7);
   }
-  if (!digitalRead(btn8)) {
+  if (!digitalRead(MouseLeft)) {
     Gamepad.press(8);
     nowState |= (uint32_t)1 << 8; 
+//    isMouseLeftUse = true;
   }else{
     Gamepad.release(8);
     nowState &= ~((uint32_t)1 << 8);
+//    isMouseLeftUse = false;
   }
 
   if (!digitalRead(btnS1)) {
@@ -176,27 +185,48 @@ void loop() {
   bool isdown = !digitalRead(bDown);
   if (isleft || isright) {
     if(isleft){
-      Gamepad.xAxis(-32760);
-      nowState |= (uint32_t)1 << 12; 
+        if(isMouseRightUse){
+          Gamepad.rxAxis(-32760);
+        }else{
+          Gamepad.xAxis(-32760);
+        }
+      
+        nowState |= (uint32_t)1 << 12; 
     }else{
-        Gamepad.xAxis(32760);
+          if(isMouseRightUse){
+            Gamepad.rxAxis(32760);
+          }else{
+            Gamepad.xAxis(32760);
+          }
         nowState |= (uint32_t)1 << 13; 
       }
   }else{
     Gamepad.xAxis(0);
+    Gamepad.rxAxis(0);
     nowState &= ~((uint32_t)1 << 12);
     nowState &= ~((uint32_t)1 << 13);
   }
   if (isup || isdown) {
     if(isdown){
-      Gamepad.yAxis(-32760);
+        if(isMouseRightUse){
+          Gamepad.ryAxis(-32760);
+        }else{
+          Gamepad.yAxis(-32760);
+        }
+      
       nowState |= (uint32_t)1 << 14; 
     }else{
-        Gamepad.yAxis(32760);
+        if(isMouseRightUse){
+            Gamepad.ryAxis(32760);
+          }else{
+            Gamepad.yAxis(32760);
+          }
+        
         nowState |= (uint32_t)1 << 15; 
       }
   }else{
     Gamepad.yAxis(0);
+    Gamepad.ryAxis(0);
     nowState &= ~((uint32_t)1 << 14);
     nowState &= ~((uint32_t)1 << 15);
   }
